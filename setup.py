1fc740f1516e53ab9e7be1c142937711a82d6252
Zach Reichert <zach.reichert@rackspace.com>
2018-07-03 13:59:25 -0700

    Bump version: 0.7.0 → 0.8.0

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ entry_points = {
 
 setup(
     name='pytest-rpc',
-    version='0.7.0',
+    version='0.8.0',
     author='rpc-automation',
     author_email='rpc-automation@rackspace.com',
     license='Apache Software License 2.0',
