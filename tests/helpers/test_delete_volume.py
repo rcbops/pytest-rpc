ee1de775ac68765013647b2d46559866962495f7
Ryan Gard <ryan.gard@rackspace.com>
2018-10-26 14:36:29 -0700

    ASC-1099 Fix 'tests.helpers.test_delete_volume' Formatting

--- tests/helpers/test_delete_volume.py
+++ tests/helpers/test_delete_volume.py
@@ -2,17 +2,17 @@
 
 """Test cases for the 'delete_volume' helper function."""
 
-# ======================================================================================================================
+# ==============================================================================
 # Imports
-# ======================================================================================================================
+# ==============================================================================
 import os
 import pytest
 import pytest_rpc.helpers
 
 
-# ======================================================================================================================
+# ==============================================================================
 # Tests
-# ======================================================================================================================
+# ==============================================================================
 def test_successful_deletion(mocker):
     """Verify delete_volume returns None when OpenStack volume has been
     successfully deleted."""
@@ -22,7 +22,8 @@ def test_successful_deletion(mocker):
     assert not pytest_rpc.helpers.delete_volume('myvolume', 'host')
 
 
-@pytest.mark.skipif('SKIP_LONG_RUNNING_TESTS' in os.environ, reason='Impatient developer is impatient')
+@pytest.mark.skipif('SKIP_LONG_RUNNING_TESTS' in os.environ,
+                    reason='Impatient developer is impatient')
 def test_failed_deletion(mocker):
     """Verify delete_volume raises an AssertionError when OpenStack volume has
     failed to successfully be deleted."""
