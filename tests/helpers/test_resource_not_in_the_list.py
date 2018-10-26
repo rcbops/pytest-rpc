04cfb9a0b357189135972458f72ee9e1ff45ea02
Ryan Gard <ryan.gard@rackspace.com>
2018-10-26 14:45:11 -0700

    ASC-1099 Fix 'tests.helpers.test_resource_is_not_in_the_list' Formatting

--- tests/helpers/test_resource_not_in_the_list.py
+++ tests/helpers/test_resource_not_in_the_list.py
@@ -5,8 +5,9 @@ import pytest_rpc.helpers
 
 
 def test_true(mocker):
-    """Verify resource_not_in_the_list returns True when _resource_in_list resolves
-    to True."""
+    """Verify resource_not_in_the_list returns True when _resource_in_list
+    resolves to True.
+    """
 
     mocker.patch('pytest_rpc.helpers._resource_in_list', return_value=True)
 
@@ -15,8 +16,9 @@ def test_true(mocker):
 
 
 def test_false(mocker):
-    """Verify resource_not_in_the_list returns False when _resource_in_list resolves
-    to False."""
+    """Verify resource_not_in_the_list returns False when _resource_in_list
+    resolves to False.
+    """
 
     mocker.patch('pytest_rpc.helpers._resource_in_list', return_value=False)
 
