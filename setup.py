5cb6b79d8f843ebc4ab83d30a6cf4a26adfc75f8
Ryan Gard <ryan.gard@rackspace.com>
2019-01-31 15:30:46 -0800

    Bump version: 1.0.1-dev0 → 1.0.1

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ entry_points = {
 
 setup(
     name='pytest-rpc',
-    version='1.0.1-dev0',
+    version='1.0.1',
     author='rpc-automation',
     author_email='rpc-automation@rackspace.com',
     license='Apache Software License 2.0',
